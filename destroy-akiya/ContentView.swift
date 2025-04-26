65e505503a9b35048f7a1be93ad7a4faefc8b53b
yu-sugimoto <yuyuyuk2002@gmail.com>
2025-04-27 07:06:46 +0900

    Unityモデルの読み込み

--- destroy-akiya/ContentView.swift
+++ destroy-akiya/ContentView.swift
@@ -13,21 +13,29 @@ struct ContentView : View {
     var body: some View {
         RealityView { content in
 
-            // Create a cube model
-            let model = Entity()
+            // Create a cube model(デフォルトのモデル)
+            let defaultModel = Entity()
             let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
             let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
-            model.components.set(ModelComponent(mesh: mesh, materials: [material]))
-            model.position = [0, 0.05, 0]
+            defaultModel.components.set(ModelComponent(mesh: mesh, materials: [material]))
+            defaultModel.position = [-0.2, 0.05, 0]
 
-            // Create horizontal plane anchor for the content
-            let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
-            anchor.addChild(model)
+            // Load Unity model (ダウンロードしたusdzファイル)
+            do {
+                let unityModel = try await Entity(named: "chair_swan")
+                unityModel.position = [0.2, 0.05, 0]
 
-            // Add the horizontal plane anchor to the scene
-            content.add(anchor)
+                // Create horizontal plane anchor for the content
+                let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
 
-            content.camera = .spatialTracking
+                anchor.addChild(defaultModel)
+                anchor.addChild(unityModel)
+
+                content.add(anchor)
+                content.camera = .spatialTracking
+            } catch {
+                print("Unityモデル読み込み失敗: \(error)")
+            }
 
         }
         .edgesIgnoringSafeArea(.all)
